357e0397f503a4c7945298e897de049f7762f1ab
Arthur <nashcassano8@gmail.com>
2025-09-20 21:47:44 -0300

    Inicializando a base de dados em sqlite

---
+++ ConsumirApi/Database/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ConsumirApi.Database;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+        
+    }
+}
