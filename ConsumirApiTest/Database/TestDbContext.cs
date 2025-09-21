01ffae9ff80156e03f593e5872e7d1522b6b4f9a
Arthur <nashcassano8@gmail.com>
2025-09-20 23:56:19 -0300

    Adicionando e finalizando testes unitários com sucesso

---
+++ ConsumirApiTest/Database/TestDbContext.cs
@@ -0,0 +1,34 @@
+﻿using ConsumirApi.Database;
+using ConsumirApi.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsumirApiTest.Database;
+
+public static class TestDbContext
+{
+    public static AppDbContext InMemoryDbContext()
+    {
+        var options = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .Options;
+        
+        var context = new AppDbContext(options);
+
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+
+        return context;
+    }
+
+    public static User CreateUser()
+    {
+        return new User
+        {
+            Id = 1,
+            Username = "teste",
+            Email = "teste@gmail.com",
+            Password = "teste123",
+            Role = 0,
+        };
+    }
+}
---
+++ ConsumirApiTest/Repository/RepositoryTests.cs
@@ -0,0 +1,77 @@
+﻿using ConsumirApi.Repository;
+using ConsumirApiTest.Database;
+
+namespace ConsumirApiTest.Repository;
+
+public class RepositoryTests
+{
+    [Fact]
+    public async Task ShouldReturnUsers()
+    {
+        var context = TestDbContext.InMemoryDbContext();
+        var repository = new UserRepository(context);
+        var user = TestDbContext.CreateUser();
+
+        await repository.AddUser(user);
+        var result = await repository.GetAll();
+        
+        Assert.NotNull(result);
+        Assert.True(result.Any());  
+    }
+    
+    [Fact]
+    public async Task ShouldReturnUserById()
+    {
+        var context = TestDbContext.InMemoryDbContext();
+        var repository = new UserRepository(context);   
+        var user = TestDbContext.CreateUser();      
+        
+        await repository.AddUser(user);
+        var result = await repository.GetById(user.Id);   
+        
+        Assert.NotNull(result);
+        Assert.Equal(user.Id, result.Id);
+    }
+
+    [Fact]
+    public async Task ShouldCreateUser()
+    {
+        var context = TestDbContext.InMemoryDbContext();
+        var repository = new UserRepository(context);       
+        var user = TestDbContext.CreateUser();
+        
+        var result = await repository.AddUser(user);
+        
+        Assert.NotNull(result);
+        Assert.Equal(user.Id, result.Id);   
+        Assert.Equal(user.Email, result.Email);
+    }
+
+    [Fact]
+    public async Task ShouldUpdateUser()
+    {
+        var context = TestDbContext.InMemoryDbContext();
+        var repository = new UserRepository(context);   
+        var user = TestDbContext.CreateUser();
+
+        await repository.AddUser(user);
+        user.Username = "testezinho";
+        var result = await repository.UpdateUser(user);  
+        
+        Assert.NotNull(result);
+        Assert.Equal(user.Username, result.Username);     
+    }
+
+    [Fact]
+    public async Task ShouldDeleteUser()
+    {
+        var context = TestDbContext.InMemoryDbContext();
+        var repository = new UserRepository(context);
+        var user = TestDbContext.CreateUser();  
+        
+        await repository.AddUser(user);
+        var result = await repository.DeleteUser(user); 
+        
+        Assert.True(result);
+    }
+}
