147d1df0b5c9da8bfdb3738461e2a457b1dd8f8f
Arthur <nashcassano8@gmail.com>
2025-09-22 16:58:08 -0300

    Adicionando Interface e Service

---
+++ FrontEnd/Interface/IUserService.cs
@@ -0,0 +1,15 @@
+﻿using ConsumirApi.DTOS;
+using ConsumirApi.Model;
+
+namespace BlazorApp1.Interface;
+
+public interface IUserService
+{
+    Task<IEnumerable<User>> GetUsers();
+    Task<User> GetUser(string username);
+    Task<User> AddUser(User user);
+    Task<User> UpdateUser(User user);
+    Task<bool> DeleteUser(int id);
+    Task<string> Login(LoginDTO dto);
+    Task<bool> Register(User user);
+}
---
+++ FrontEnd/Service/UserService.cs
@@ -0,0 +1,49 @@
+﻿using BlazorApp1.Interface;
+using ConsumirApi.DTOS;
+using ConsumirApi.Model;
+
+namespace BlazorApp1.Service;
+
+public class UserService : IUserService 
+{
+    private readonly HttpClient _client;
+
+    public UserService(HttpClient client)
+    {
+        _client = client;
+    }
+    public Task<IEnumerable<User>> GetUsers()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> GetUser(string username)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> AddUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<User> UpdateUser(User user)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> DeleteUser(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<string> Login(LoginDTO dto)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> Register(User user)
+    {
+        throw new NotImplementedException();
+    }
+}
