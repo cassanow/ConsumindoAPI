24b82a5c2f18459c1261e01705cb331a97780d95
Arthur <nashcassano8@gmail.com>
2025-09-22 14:06:09 -0300

    Adicionando Swagger e vendo se os endpoints funcionaram corretamente

--- ConsumirApi/Program.cs
+++ ConsumirApi/Program.cs
@@ -1,4 +1,6 @@
 using ConsumirApi.Database;
+using ConsumirApi.Interface;
+using ConsumirApi.Repository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,12 +16,17 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.MapControllers();   
